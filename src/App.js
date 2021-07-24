0345e7b2c4e4c3f002565e71aca434d32be2eb43
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-07-23 20:17:33 -0500

    Nesting example

--- src/App.js
+++ src/App.js
@@ -1,73 +1,53 @@
 import "./App.css";
 import React from "react";
-import { Router, Link, useNavigate } from "@reach/router";
+import { Router, Link } from "@reach/router";
 
 function App() {
-  let inSesion = false;
-
-  const Header = (props) => {
-    const navigate = useNavigate();
-
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        {props.children}
-        <br />
-        <button onClick={() => navigate("/")}>Regresar</button>
-      </div>
-    );
-  };
-
-  {
-    /**
-  // or with hooks
-  const Invoice = () => {
-    const params = useParams()
-    return (
-      <div>
-        <h1>Invoice {params.invoiceId}</h1>
-      </div>
-    )
-  }
-  */
-  }
-
-  const Home = () => (
-    <Header title="Home">
-      <nav>
-        <Link to="/">Home</Link> | <Link to="consulta">Consulta</Link> |{" "}
-        <Link to="usuario/6">Usuario</Link> | <Link to="sesion">Perfil</Link>
-      </nav>
-    </Header>
+  const Invoice = (props) => (
+    <div>
+      <h2>Invoice {props.invoiceId}</h2>
+    </div>
   );
 
-  const Consulta = (props) => (
-    <Header title="Consulta">Soy la página de consulta</Header>
+  const Home = () => <h2>Soy el Home</h2>;
+
+  const Dashboard = () => <h2>Soy el Dashboard</h2>;
+
+  const Invoices = (props) => (
+    <div>
+      <h2>Invoices</h2>
+      <ul>
+        <li>
+          <Link to="/invoices/123">Invoice 123</Link>
+        </li>
+        <li>
+          <Link to="/invoices/abc">Invoice ABC</Link>
+        </li>
+      </ul>
+
+      {props.children}
+    </div>
   );
-
-  const Usuario = (props) => (
-    <Header title="Usuario">
-      Usuario {props.userId}
-      <button onClick={() => props.navigate("../66")}>Ver otro usuario</button>
-    </Header>
-  );
-
-  const NotFound = () => (
-    <Header title="Not Found">Sorry, nothing here.</Header>
-  );
-
-  const Perfil = () => <Header title="Perfil">Soy Perfil</Header>;
-
-  const NoSesion = () => <Header title="NoSesion">Soy NoSesion</Header>;
 
   return (
-    <Router>
-      <Home path="/" />
-      <Consulta path="consulta" />
-      <Usuario path="usuario/:userId" />
-      {inSesion ? <Perfil path="sesion" /> : <NoSesion path="sesion" />}
-      <NotFound default />
-    </Router>
+    <div>
+      <h1>Tutorial!</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        {" | "}
+        <Link to="dashboard">Dashboard</Link>
+        {" | "}
+        <Link to="invoices">Invoices</Link>
+      </nav>
+
+      <Router>
+        <Invoices path="invoices">
+          <Invoice path=":invoiceId" />
+        </Invoices>
+        <Home path="home" />
+        <Dashboard path="dashboard" />
+      </Router>
+    </div>
   );
 }
 
