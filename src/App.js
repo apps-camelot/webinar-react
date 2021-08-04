9a372c649ac1e8f24e5b2e532ca1b8de79034df4
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-08-04 18:26:47 -0500

    Ejemplo input

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import "./App.css";
-import React, { useRef } from "react";
+import React from "react";
 
 function App() {
-  const form = useRef(null);
-
-  const submit = (e) => {
-    console.log(form.current);
-
-    e.preventDefault();
-    const data = new FormData(form.current);
-
-    let datos = Object.fromEntries(data);
-
-    console.log(datos);
-
-    fetch("/api", { method: "POST", body: data })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  };
-
   return (
     <div>
-      <form ref={form} onSubmit={submit}>
-        <input type="text" name="nombre" />
-        <input type="text" name="apellidos" />
-
-        <textarea name="contenido">Soy un text area</textarea>
-
-        <select name="genero">
-          <option value="femenino">Femenino</option>
-          <option value="masculino">Masculino</option>
-        </select>
-
-        <input type="submit" name="Enviar" />
-      </form>
+      <input
+        id="nombre"
+        value={nombre}
+        placeholder="Escribe..."
+        onChange={(event) => {
+          setNombre(event.target.value);
+        }}
+      />
     </div>
   );
 }
